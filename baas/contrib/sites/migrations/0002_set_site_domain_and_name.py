98ce6f8bf32aecece92a682d2a11f18b5200332b
jochemb <j.bijlard@gmail.com>
2016-06-14 23:41:39 +0200

    Adjust name and domain (email)

--- baas/contrib/sites/migrations/0002_set_site_domain_and_name.py
+++ baas/contrib/sites/migrations/0002_set_site_domain_and_name.py
@@ -17,8 +17,8 @@ def update_site_forward(apps, schema_editor):
     Site.objects.update_or_create(
         id=settings.SITE_ID,
         defaults={
-            'domain': 'example.com',
-            'name': 'baas'
+            'domain': 'arborist-test-trait.thehyve.net',
+            'name': 'Boris as a Service'
         }
     )
 
